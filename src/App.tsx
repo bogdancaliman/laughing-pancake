a0293d11e6dfa014a8f716f2eb6a31d79bfeccba
Bogdan <bogdan.caliman112@gmail.com>
2023-07-04 15:22:44 +0300

    feat: added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./routes/home/home.component";
+import Messages from "./routes/messages/messages.component";
+import Bookmarks from "./routes/bookmarks/bookmarks.component";
+import Explore from "./routes/explore/explore.component";
+import Lists from "./routes/lists/lists.component";
+import Notifications from "./routes/notifications/notifications.component";
+import Profile from "./routes/profile/profile.component";
+
 function App() {
-  return <div className="text-3xl font-mono underline">Hello World!</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/messages" element={<Messages />} />
+      <Route path="/bookmarks" element={<Bookmarks />} />
+      <Route path="/explore" element={<Explore />} />
+      <Route path="/lists" element={<Lists />} />
+      <Route path="/notifications" element={<Notifications />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
 }
 
 export default App;
