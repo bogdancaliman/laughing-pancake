5492fd2b56a02cef7b71b2b0429e129b821e7ece
Bogdan <bogdan.caliman112@gmail.com>
2023-07-09 15:35:33 +0300

    refac: refactoring navigation and home component

--- src/components/navigation-button/navigation-button.component.tsx
+++ src/components/navigation-button/navigation-button.component.tsx
@@ -1,3 +1,5 @@
+import HomeIcon from "../../../public/navigation/home.svg";
+
 interface Text {
   text: string;
 }
@@ -5,7 +7,10 @@ interface Text {
 function NavigationButton({ text }: Text) {
   return (
     <div className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-full shadow-md">
-      {text}
+      <div className="flex gap-2">
+        <img src={HomeIcon} alt="Home" className="h-6 w-6" />
+        <span className="hidden lg:inline">{text}</span>
+      </div>
     </div>
   );
 }
--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -1,5 +1,23 @@
 function Home() {
-    return <div>Home</div>
+  return (
+    <div>
+      <div className="sticky top-0 bg-white bg-opacity-50 py-4">
+        Sticky Top Block
+      </div>
+      <div className="bg-red-300 py-32 border border-black">
+        Content with Red Background
+      </div>
+      <div className="bg-red-300 py-32 border border-black">
+        Content with Red Background
+      </div>
+      <div className="bg-red-300 py-32 border border-black">
+        Content with Red Background
+      </div>
+      <div className="bg-red-300 py-32 border border-black">
+        Content with Red Background
+      </div>
+    </div>
+  );
 }
 
 export default Home;
